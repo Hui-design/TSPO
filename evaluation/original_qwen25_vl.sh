67f32df9cf6834672a9124cf69a8a4efd520a471
Canhui Tang <69513731+Hui-design@users.noreply.github.com>
2025-08-22 16:56:42 +0800

    Update original_qwen25_vl.sh

--- evaluation/original_qwen25_vl.sh
+++ evaluation/original_qwen25_vl.sh
@@ -1,12 +1,15 @@
 dataset_name=$1
 experiment_name=$2
-
+json_name=''
 if [ "$dataset_name" = "LongVideoBench" ]; then
     task_name="longvideobench_val_v"
+    json_name="lvb_val"
 elif [ "$dataset_name" = "VideoMME" ]; then
     task_name="videomme"
+    json_name="videomme"
 elif [ "$dataset_name" = "MLVU" ]; then
     task_name="mlvu_dev"
+    json_name="mlvu"
 else
     echo "Our code doesn't support this dataset now"
     exit 1
@@ -15,7 +18,7 @@ fi
 save_name="${dataset_name}"
 export HF_HOME="evaluation/data/videos/${dataset_name}"
 # export LMMSJSON="evaluation/jsons_idx/${experiment_name}_${dataset_name}_frameIdx.json"
-export LMMSJSON="evaluation/jsons/${dataset_name}.json"
+export LMMSJSON="evaluation/jsons/${json_name}.json"
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --num_processes 8 --main_process_port 12345 -m lmms_eval \
     --model qwen2_5_vl_tspo \
